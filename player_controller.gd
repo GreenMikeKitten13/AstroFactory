37cc14beea36594163bb904d1e541569cfdc188d
Denny3232 <sdgca.co@gmail.com>
2025-04-03 20:45:59 +0200

    wer
    wer

---
+++ player_controller.gd
@@ -0,0 +1,14 @@
+extends MeshInstance3D
+
+@export var speed: float = 5.0
+
+func _process(delta):
+
+	if Input.is_action_pressed("Forward"): 
+		global_position.z += speed * delta
+	if Input.is_action_pressed("Backward"):  
+		global_position.z -= speed * delta
+	if Input.is_action_pressed("Left"):  
+		global_position.x -= speed * delta
+	if Input.is_action_pressed("Right"):  
+		global_position.x += speed * delta
