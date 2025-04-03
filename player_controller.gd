84637ccab4e31d1c58d7f11c660ef6d6479796d4
Denny3232 <sdgca.co@gmail.com>
2025-04-03 21:06:44 +0200

    player_controller (wasd) [alpha, beta, gamma, delta.....]
    - added movement with w, a , s and d

--- player_controller.gd
+++ player_controller.gd
@@ -1,6 +1,6 @@
 extends MeshInstance3D
 
-@export var speed: float = 5.0
+@export var speed: float = 10.0
 
 func _process(delta):
 
@@ -9,6 +9,6 @@ func _process(delta):
 	if Input.is_action_pressed("Backward"):  
 		global_position.z -= speed * delta
 	if Input.is_action_pressed("Left"):  
-		global_position.x -= speed * delta
-	if Input.is_action_pressed("Right"):  
 		global_position.x += speed * delta
+	if Input.is_action_pressed("Right"):  
+		global_position.x -= speed * delta
