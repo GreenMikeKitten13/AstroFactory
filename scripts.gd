d411c180a68904612e366ae6beb96e32e8da00dc
GreenMikeKitten13 <gass.maik13@gmail.com>
2025-04-05 16:18:58 +0200

    alpha beta gamma demo 0.15

--- scripts.gd
+++ scripts.gd
@@ -11,13 +11,18 @@ func _ready():
 	noise.seed = randi()
 	noise.noise_type = FastNoiseLite.TYPE_PERLIN
 	noise.frequency = 0.1
-	for yCooridnate in gridSize/2:
-		if yCooridnate == gridSize/2 - 1: prefab = $"../GrassBlock"
+	for yCoordinate in gridSize/2:
+		
+		if yCoordinate <= gridSize/2 -7: prefab = $"../StoneBody" 
+		else: prefab = $"../DirtBody" 
+		
+		if yCoordinate == gridSize/2 - 1: prefab = $"../GrassBody"
+		
 		for zCoorindate in gridSize:
 			for xCoordinate in gridSize:
 				var height = noise.get_noise_2d(xCoordinate, zCoorindate) * 1  # Scale height
 				var newCopper = prefab.duplicate()
-				newCopper.position = Vector3(xCoordinate,-9 + height + yCooridnate,zCoorindate)
+				newCopper.position = Vector3(xCoordinate,-9 + height + yCoordinate,zCoorindate)
 				#var degree = angles[randi() % angles.size()]
 				#newCopper.rotation_degrees.y = degree
 				add_child(newCopper)
