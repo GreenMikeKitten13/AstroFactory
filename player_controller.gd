0b5ca4ef8cfed571d7350c2c35ebed21cc085b74
GreenMikeKitten13 <gass.maik13@gmail.com>
2025-04-05 11:01:48 +0200

    alpha beta gamma demo 0.14

--- player_controller.gd
+++ player_controller.gd
@@ -10,6 +10,7 @@ func _process(delta):
 	var direction = Vector3.ZERO
 
 	if Input.is_action_pressed("Forward"): 
+		#$"../LilMan/CharacterBody3D".move_and_slide() 
 		direction.z += 1
 	if Input.is_action_pressed("Backward"):  
 		direction.z -= 1
@@ -17,7 +18,11 @@ func _process(delta):
 		direction.x += 1
 	if Input.is_action_pressed("Right"):  
 		direction.x -= 1
-		
+	if Input.is_action_pressed("Escape"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		Input.set_mouse_mode(Input.MOUSE_MODE_MAX)
+	
+	
 	direction = direction.normalized()
 	direction = direction.rotated(Vector3.UP, rotation.y)
 	global_position += direction * speed * delta
@@ -27,5 +32,5 @@ func _input(event):
 	if event is InputEventMouseMotion:
 		rotation.y += event.relative.x * -0.01
 		rotation.x = max(min(rotation.x + event.relative.y * 0.005, deg_to_rad(90)), deg_to_rad(-90))
-		print(rad_to_deg(rotation.x))
+		#print(rad_to_deg(rotation.x))
 		$"../LilMan".rotation.y += event.relative[0] * -0.01
--- scripts.gd
+++ scripts.gd
@@ -1,8 +1,23 @@
 extends Node
 
-@onready var copperPrefab = $"../CopperBody" # Replace with the actual node path
+@onready var prefab = $"../DirtBody"
+var gridSize := 25
+var noise = FastNoiseLite.new()
+#var angles = [-90, 90, 180]
+
+#var degree:Vector3
 
 func _ready():
-	var newCopper := copperPrefab.duplicate() # Creates a copy
-	newCopper.position += Vector3(2, 0, 0) # Move slightly to avoid overlap
-	add_child(newCopper) # Add to the scene
+	noise.seed = randi()
+	noise.noise_type = FastNoiseLite.TYPE_PERLIN
+	noise.frequency = 0.1
+	for yCooridnate in gridSize/2:
+		if yCooridnate == gridSize/2 - 1: prefab = $"../GrassBlock"
+		for zCoorindate in gridSize:
+			for xCoordinate in gridSize:
+				var height = noise.get_noise_2d(xCoordinate, zCoorindate) * 1  # Scale height
+				var newCopper = prefab.duplicate()
+				newCopper.position = Vector3(xCoordinate,-9 + height + yCooridnate,zCoorindate)
+				#var degree = angles[randi() % angles.size()]
+				#newCopper.rotation_degrees.y = degree
+				add_child(newCopper)
