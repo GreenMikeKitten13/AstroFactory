08cad5da764b1dea52a436c5041c19ae514f2902
GreenMikeKitten13 <gass.maik13@gmail.com>
2025-04-19 19:55:27 +0200

    alpha beta gamma 0.9

--- scripts/sentry_behavior.gd
+++ scripts/sentry_behavior.gd
@@ -4,16 +4,34 @@ var bodies_in_range := []
 var last_shot_time : float = 0
 var can_shoot : bool = true
 var looking_pos : Vector3 = Vector3(0, 0, 0)
+@onready var bullets: Node3D = %Bullets
+@onready var bullet:RigidBody3D = bullets.get_child(0)
 
 func _ready():
-	$Timer.wait_time = 1.0 # in sec
+	
+	bullet.physics_material_override = PhysicsMaterial.new()
+	bullet.physics_material_override.bounce = 0
+	bullet.physics_material_override.friction = 0
+	bullet.collision_layer = 1
+	bullet.collision_mask = 1
+	$Timer.wait_time = 1.0
 	$Timer.start()
 
 func shoot():
 	if can_shoot:
 		can_shoot = false
-	$Timer.start()
+		bullet.set_process(true)
+		bullet.show()
+		bullet.global_transform = global_transform
+		bullet.reparent($"../../badGameEngineDesigna")
+		bullet.linear_velocity = bullet.global_transform.basis.z.normalized() * -50 
+		$Timer.start()
+		await $Timer.timeout
+		bullet.reparent(bullets)
+		bullet.set_process(false)
 	
+	$Timer.start()
+
 func _on_timer_timeout():
 	can_shoot = true
 
@@ -40,3 +58,16 @@ func _process(_delta):
 			
 	
 		
+
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	if body.name == "PlayerBody":
+		bullet.linear_velocity = Vector3.ZERO
+		bullet.set_process(false)
+		body.set_meta("health", body.get_meta("health") - 5)
+		bullet.hide()
+		await get_tree().create_timer(0.5).timeout
+		bullet.reparent(bullets)
+
+func reparentBullet():
+	bullet.reparent(bullets)
